b6cd9dcaf6ff3331dda0c3a6428ca903be41320d
imakshat47 <imakshat47@gmail.com>
2025-08-26 22:29:03 +0530

    Update from Laptop by ImAkshat47 - voice_frontend/src/i18n.js at 2025-08-26_22-23-22

---
+++ voice_frontend/src/i18n.js
@@ -0,0 +1,19 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: { translation: { /* English translations */ } },
+  hi: { translation: { /* Hindi translations */ } },
+  mr: { translation: { /* Marathi translations */ } },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en", // default language
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
